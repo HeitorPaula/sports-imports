33d1a1ab1f5a3ac170ffbcb593e12c362581e0c5
Heitor de Paula <76218700+HeitorPaula@users.noreply.github.com>
2025-08-23 23:25:04 -0300

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: 'sports-imports',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
