e9924c7ccfba8ebaa826bedb596a24b689e6a078
Heitor de Paula <76218700+HeitorPaula@users.noreply.github.com>
2025-08-25 16:38:09 -0300

    Corrige deploy do Vite para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-  import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sports-imports/', // Caminho correto para o GitHub Pages
-  build: {
-    outDir: 'build', // Onde o Vite vai gerar os arquivos
-  },
+  base: "/sports-imports/", // 👈 Caminho do repositório exato
 });
