34362057b3dc06c92ff4eb3fa149cb7ca7647084
Heitor de Paula <76218700+HeitorPaula@users.noreply.github.com>
2025-08-26 18:23:46 -0300

    fix: ajustar base do vite e deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Substitua "sports-imports" pelo nome exato do seu repositório
 export default defineConfig({
   plugins: [react()],
-  base: "/sports-imports/",
+  base: "/sports-imports/", // <-- nome exato do repositório
+  build: {
+    outDir: "dist",
+  },
 });
